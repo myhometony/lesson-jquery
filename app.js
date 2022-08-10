e08d6ea1811a10dcca888752416cdb7b60d43dd2
USER <102310350+myhometony@users.noreply.github.com>
2022-08-11 08:53:36 +0900

    [Add]app.css

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
-$(document).ready(function(){//(document).readyは省略可
-  $("body").html("<h1>Hello jQuery!!</h1>");
-});
+//$(document).ready(function(){//(document).readyは省略可
+  //$("body").html("<h1>Hello jQuery!!</h1>");
+//});
 //$( セレクタ ).メソッド ( パラメータ ) ;
 //bodyタグ内のHTMLを、<h1>Hello JQuery!!</h1>で書き換えなさい
+
+$(function(){
+  $('.box1').css({
+    'background-color': '#0000FF',
+    'height': '100px'
+  });
+});
