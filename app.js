efed81b841824902f09c1f734f53084a904b9179
USER <102310350+myhometony@users.noreply.github.com>
2022-08-12 23:55:46 +0900

    [Update]app

--- app.js
+++ app.js
@@ -5,8 +5,11 @@
 //bodyタグ内のHTMLを、<h1>Hello JQuery!!</h1>で書き換えなさい
 
 $(function(){
-  $('.box1').css({
-    'background-color': '#0000FF',
-    'height': '100px'
+  $(".box1").slideDown(function(){
+      $('.box1').css({
+        'background-color': '#0000FF',
+         'height': '100px',
+         "weight":"200px"
+      }).slideUp();
   });
 });
